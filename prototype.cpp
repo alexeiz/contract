2f68f784e9b95177a7a19f3ac9ba04d5505e2a3b
Alexei Zakharov <alexeiz@gmail.com>
2013-03-22 15:20:31 -0400

    a prototype for a declarative contract library

---
+++ prototype.cpp
@@ -0,0 +1,95 @@
+struct contract_context
+{
+    bool precondition;
+    bool postcondition;
+    bool invariant;
+};
+
+template <typename T>
+struct concrete_contract
+{
+    concrete_contract(T func)
+        : func_(func)
+        , context_{true, false, true}
+    {
+        register_context(context_);
+        check_contract(func_);
+    }
+
+    ~concrete_contract()
+    {
+        context_ = {false, true, true};
+        check_contract(func_);
+    }
+
+    T func_;
+    contract_context context_;
+};
+
+struct contract_maker
+{
+    template <typename T>
+    concrete_contract<T> operator+(T func)
+    {
+        return concrete_contract<T>(func);
+    }
+};
+
+#define contract                                                          \
+    auto contract_obj = contract_maker() + [&]{ class_contract(); } + [&] \
+
+class account
+{
+public:
+    account(int deposit)
+        : balance_(deposit)
+    {
+        contract {}
+    }
+
+    ~account()
+    {
+        contract {}
+    }
+
+    int withdraw(int ammount)
+    {
+        int withdrawn = 0;
+
+        contract
+        {
+            precondition(ammount >= 0);
+            postcondition(withdrawn >= 0);
+        }
+
+        withdrawn = ammount >= balance_ ? balance_ : ammount;
+        balance_ -= withdrawn;
+        return withdrawn;
+    }
+
+    int deposit(int ammount)
+    {
+        contract
+        {
+            precondition(ammount >= 0);
+        }
+
+        balance_ += ammount;
+        return balance_;
+    }
+
+    int balance() const
+    {
+        contract {}
+        return balance_;
+    }
+
+private:
+    void class_contract() const
+    {
+        invariant(balance_ >= 0);
+    }
+
+private:
+    int balance_;
+};
