dc990a0307f27e7c0175498d1c7df67671e88b0d
Alexei Zakharov <alexeiz@gmail.com>
2013-04-04 16:25:58 -0400

    removed outdated files, gave new source files better names

--- test/usage.cpp
+++ test/usage.cpp
@@ -1,21 +1,122 @@
 #include <contract.h>
 
-#include <cstddef>
+#include <iostream>
 #include <cstring>
+#include <cassert>
+
+// example 1: class and method contracts
+
+class account
+{
+public:
+    account(int deposit)
+        : balance_(-1)
+    {
+        contract(ctor) { precondition(deposit >= 0); };
+
+        balance_ = deposit;
+    }
+
+    ~account()
+    {
+        int closing_balance = balance_;
+        contract(dtor) { postcondition(closing_balance >= 0); };
+
+        balance_ = -1;
+    }
+
+    int withdraw(int ammount)
+    {
+        int withdrawn = 0;
+
+        contract(meth)
+        {
+            precondition(ammount >= 0);
+            postcondition(withdrawn >= 0);
+        };
+
+        withdrawn = ammount >= balance_ ? balance_ : ammount;
+        balance_ -= withdrawn;
+        return withdrawn;
+    }
+
+    int deposit(int ammount)
+    {
+        contract(meth) { precondition(ammount >= 0); };
+
+        balance_ += ammount;
+        return balance_;
+    }
+
+    int balance() const
+    {
+        contract(meth) {};
+        return balance_;
+    }
+
+private:
+    contract(class) { invariant(balance_ >= 0); };
+
+private:
+    int balance_;
+};
+
+// example 2: function contracts
+
+account make_account_with_fee(int balance)
+{
+    int const fee = 5;
+    contract(fun) { precondition(balance > fee); };
+
+    return account(balance - fee);
+}
+
+// example 3: function contracts
 
 std::size_t my_strlen(char const * str)
 {
-    precondition(str, "invalid argument");
-
     std::size_t len = 0;
+
+    contract(fun)
+    {
+        precondition(str && "invalid argument");
+        postcondition(len == std::strlen(str) && "incorrect return value");
+    };
+
     for (char const * p = str; *p; ++len, ++p)
         ;
 
-    postcondition(len == std::strlen(str), "incorrect return value");
     return len;
+}
+
+// example 4: custom contract violation handler
+
+contract::violation_handler old_handler;
+void my_contract_violation_handler(contract::type type,
+                                   char const * message,
+                                   char const * expr,
+                                   char const * func,
+                                   char const * file,
+                                   std::size_t line)
+{
+    std::cerr << "in my_contract_violation_handler:\n";
+    old_handler(type, message, expr, func, file, line);
 }
 
 int main()
 {
+    // example 4
+    old_handler = contract::set_handler(my_contract_violation_handler);
+
+    // example 3
     my_strlen("a string");
+
+    // example 1
+    account acc(100);
+    acc.withdraw(10);
+    acc.deposit(20);
+    assert(acc.balance() == 110);
+
+    // example 2
+    acc = make_account_with_fee(15);
 }
