f253db56276190441ad8cad4729cb87def7eff0d
Alexei Zakharov <alexeiz@gmail.com>
2025-05-27 14:16:00 -0400

    use boost 1.88

--- cmake/Deps.cmake
+++ cmake/Deps.cmake
@@ -3,9 +3,9 @@ include(cmake/CPM.cmake)
 
 CPMAddPackage(
   NAME Boost
-  VERSION 1.87.0
-  URL https://github.com/boostorg/boost/releases/download/boost-1.87.0/boost-1.87.0-cmake.tar.xz
-  URL_HASH SHA256=7da75f171837577a52bbf217e17f8ea576c7c246e4594d617bfde7fafd408be5
+  VERSION 1.88.0
+  URL https://github.com/boostorg/boost/releases/download/boost-1.88.0/boost-1.88.0-cmake.tar.xz
+  URL_HASH SHA256=f48b48390380cfb94a629872346e3a81370dc498896f16019ade727ab72eb1ec
   OPTIONS "BOOST_ENABLE_CMAKE ON" "BOOST_INCLUDE_LIBRARIES test"
 )
 
