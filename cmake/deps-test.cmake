09ed091da37be96ccea7ad45af7b48edd669ac17
Alexei Zakharov <alexeiz@gmail.com>
2025-08-12 21:26:59 -0400

    allow building without tests and test dependencies

---
+++ cmake/deps-test.cmake
@@ -0,0 +1,10 @@
+CPMFindPackage(
+  NAME Boost
+  VERSION 1.88.0
+  FIND_PACKAGE_ARGUMENTS CONFIG
+  URL https://github.com/boostorg/boost/releases/download/boost-1.88.0/boost-1.88.0-cmake.tar.xz
+  URL_HASH SHA256=f48b48390380cfb94a629872346e3a81370dc498896f16019ade727ab72eb1ec
+  OPTIONS "BOOST_ENABLE_CMAKE ON" "BOOST_INCLUDE_LIBRARIES test"
+)
+
+list(APPEND LIBRARIES Boost::unit_test_framework)
